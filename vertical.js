cdcf848366224cb2cc795509e249312e25194d4e
frogfiction <60366027+frogfiction@users.noreply.github.com>
2020-02-14 18:13:30 -0800

    Create vertical.js

---
+++ vertical.js
@@ -0,0 +1,30 @@
+var ballX = 300;
+var ballY = 0;
+var ballSpeed = 5;
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(100,200,220);
+  
+  noStroke ();
+  fill (10,100);
+  ellipse(ballX, ballY, 50);
+  
+    noStroke ();
+  fill (10,100);
+  ellipse(100, ballY, 50);
+  
+
+  ballY = ballY + ballSpeed;
+  
+  if (ballY > height) {
+    ballSpeed = -ballSpeed;
+  }
+  
+  if (ballY < 0) {
+    ballSpeed = -ballSpeed;
+  }
+}
